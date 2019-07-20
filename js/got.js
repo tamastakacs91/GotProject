de3b8f992ad303644a5b7abc893e538bc807473b
Tamás Takács <tamas.takacs91@gmail.com>
2019-07-20 14:58:36 +0200

    élő karakterek sortolva megjelnítve

--- js/got.js
+++ js/got.js
@@ -1,5 +1,6 @@
 const gotData = {
   data: [],
+  aliveCharacters: [],
 
   init() {
     this.findAll();
@@ -22,31 +23,52 @@ const gotData = {
     request.send();
   },
 
-  setData(userData) {
-    this.data = JSON.parse(userData);
+  setData(characterData) {
+    this.data = JSON.parse(characterData);
+    this.getAliveCharacters();
+    this.sortAliveCharacters();
     this.showAliveCharacters();
+  },
+
+  getAliveCharacters() {
+    for (let i = 0; i < this.data.length; i += 1) {
+      if (this.data[i].dead !== true) {
+        this.aliveCharacters.push(this.data[i]);
+      }
+    }
+    return this.aliveCharacters;
+  },
+
+  sortAliveCharacters() {
+    this.aliveCharacters.sort((a, b) => {
+      const nameA = a.name;
+      const nameB = b.name;
+
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
   },
 
   showAliveCharacters() {
     let characterDiv = '';
-    let aliveCharacters = 0;
-    for (let i = 0; i < this.data.length; i += 1) {
-      if (this.data[i].dead !== true) {
-        aliveCharacters += 1;
-        characterDiv += this.createCharacterDivs(this.data[i]);
-      }
+    for (let i = 0; i < this.aliveCharacters.length; i += 1) {
+      characterDiv += this.createCharacterDivs(this.aliveCharacters[i]);
     }
-    console.log(aliveCharacters);
     this.elements.characterDivs.innerHTML += characterDiv;
   },
 
   createCharacterDivs(character) {
     return `
-    <div>
-      <div>
+    <div class="character-div--unit">
+      <div class="character-div--portrait">
         <img src=${character.portrait} alt=${character.name}>
      </div>
-      <div>
+      <div class="character-div--name">
         <p>${character.name}</p>
       </div>
     </div>
